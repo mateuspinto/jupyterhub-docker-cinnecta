72a2a9065e5b1fa5b5cfd73833c2fb4c6084cd8c
Mateus Silva <mateus.p.silva@ufv.br>
2019-11-19 21:39:29 +0000

    final

--- jupyterhub/docker-entrypoint.sh
+++ jupyterhub/docker-entrypoint.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+for f in /home/*/; do adduser -q --gecos '""' --disabled-password ${f:6:-1}; done
+for f in /home/*/; do chown -R ${f:6:-1} "$f"; done
+chmod -R 0777 /home
 chmod -R 0777 /shared
 jupyterhub --ip 0.0.0.0
--- jupyterhub/jupyterhub_config.py
+++ jupyterhub/jupyterhub_config.py
@@ -1,10 +1,10 @@
 from oauthenticator.github import LocalGitHubOAuthenticator
 c.JupyterHub.authenticator_class = LocalGitHubOAuthenticator
 
-c.LocalGitHubOAuthenticator.oauth_callback_url = 'http://localhost:8000/hub/oauth_callback'
-c.LocalGitHubOAuthenticator.client_id = 'a631fd9a2b3fa83bcaf2'
-c.LocalGitHubOAuthenticator.client_secret = '5d4fce13e4b45edb64c7cfa3d2cf84b5ba2dd014'
+c.LocalGitHubOAuthenticator.oauth_callback_url = 'http://3.80.140.38:8000/hub/oauth_callback'
+c.LocalGitHubOAuthenticator.client_id = 'a8a26087af2fd6b34a8d'
+c.LocalGitHubOAuthenticator.client_secret = '6e4ac12c347bebee7b664645087b2e56e4b4306e'
 
 c.LocalGitHubOAuthenticator.create_system_users = True
-#c.Authenticator.whitelist = {'peter','kendra'}
-c.Authenticator.admin_users = {'mateusps10','leandrolazaro'}
+c.Authenticator.whitelist = {'LeonardoJAlves','gcoimbra','RaissaPapinii','wandella'}
+c.Authenticator.admin_users = {'matelementalista','fabaguiarsilva'}
